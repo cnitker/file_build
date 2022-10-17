dca5763aedefe8d615318451b6e2d5acdb3044e8
no-body <no_body@qq.com>
2022-10-17 21:49:55 +0800

    project init

---
+++ src/config.php
@@ -0,0 +1,12 @@
+<?php
+//此处配置各应用基类控制器
+return [
+    'admin' => [
+        'filename' => 'AdminBaseController',
+        'namespace' => 'app\admin\controller\AdminBaseController',
+    ],
+    'api' => [
+        'filename' => 'ApiBaseController',
+        'namespace' => 'app\api\controller\ApiBaseController',
+    ],
+];
